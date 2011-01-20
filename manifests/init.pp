12a96ad9a273c8b991607ae779c12e95dd3f84da
Erik LaBianca <erik.labianca@gmail.com>
2011-01-20 00:06:55 -0500

    added proxy configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -67,6 +67,22 @@ class nginx  {
             notify => Service[nginx]
         }
     }
+
+    define proxy ($server_name="localhost", $server_path="/", $port=80, $proxy_url="http://127.0.0.1/", $conf_source="nginx/proxy.conf.erb") {
+        file { "/etc/nginx/sites-available/${name}.conf":
+            content => template($conf_source),
+            ensure => present,
+            notify => Service[nginx],
+        }
+        file { "/etc/nginx/sites-enabled/${name}.conf":
+            ensure => link,
+            target => "/etc/nginx/sites-available/${name}.conf",
+            notify => Service[nginx]
+        }
+    }
+}
+
+
 }
 
 
